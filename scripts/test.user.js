692d7e4f3a5a373442fc4f955511967f836fa313
David Reignier <david.reignier@gmail.com>
2014-02-28 10:46:19 +0100

    Test again

---
+++ scripts/test.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name          Test scripts
+// @namespace     https://github.com/dreignier/web-hacks
+// @description   Just a test script, don't mind me
+// @include       *
+// @version       1.0
+// ==/UserScript==
+
+console.log('This is a test !');
