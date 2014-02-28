30560657137a0b5fa906b9cdb2ed219574289b0c
David Reignier <david.reignier@gmail.com>
2014-02-28 09:56:55 +0100

    Just a test, don't mind me.

---
+++ scripts/test.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Test
+// @namespace   magus.battle.net.forum
+// @description %description%
+// @include     http*://*.battle.net/*/fr/forum/
+// @exclude     %exclude%
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+console.log('coucou !');
